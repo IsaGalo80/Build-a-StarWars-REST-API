e17ad085e2c3caf51855751f5d83c1f8f9e8324d
IsaGalo80 <isagalo1980@outlook.com>
2022-08-02 17:55:37 +0000

    fata hacer

--- src/models.py
+++ src/models.py
@@ -2,15 +2,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-favoritosPerson = db.Table('favoritosPerson',
-    db.Column('user_id', db.Integer, db.ForeignKey('User.id'), primary_key=True),
-    db.Column('personajes_id', db.Integer, db.ForeignKey('Personajes.id'), primary_key=True)
+favoritos = db.Table('favoritos',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('personajes_id', db.Integer, db.ForeignKey('personajes.id'), primary_key=True),
+    db.Column('planetas_id', db.Integer, db.ForeignKey('planetas.id'), primary_key=True)
 )
 
-favoritosPlanet = db.Table('favoritosPlanet',
-    db.Column('user_id', db.Integer, db.ForeignKey('User.id'), primary_key=True),
-    db.Column('planetas_id', db.Integer, db.ForeignKey('Planetas.id'), primary_key=True)
-)
+# favoritosPlanet = db.Table('favoritosPlanet',
+#     db.Column('user_id', db.Integer, db.ForeignKey('User.id'), primary_key=True),
+# )
+class Favorito(db.Model):
+    __tablename__ = 'favorito'
+    id = db.Column(db.Integer, primary_key=True)
 
 
 class User(db.Model):
@@ -19,14 +22,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(300), unique=False, nullable=False)
-    favoritosPerson = db.relationship(personajes,
-                    secondary=favoritosPerson,
-                    lazy='subquery',
-                    backref=db.backref('usuarios', lazy=True))
-    favoritosPlanet = db.relationship(Planetas,
-                    secondary=favoritosPlanet,
-                    lazy='subquery',
-                    backref=db.backref('usuarios', lazy=True))
+    favoritos = db.relationship('Favorito',secondary= favoritos)           
     
     # user = db.relationship("User", secondary="favoritosPlanetPer")
 def __repr__(self):
@@ -37,13 +33,13 @@ def serialize(self):
             "id": self.id,
             "password": self.password,
             "email": self.email,
-            "favoritosPerson": self.obtener_favoritosPerson(),
+            "Favoritos": self.obtener_Favoritos(),
             "favoritosPlanet": self.obtener_favoritosPlanet(),
             # do not serialize the password, its a security breach
         }
     
-def obtener_favoritosPerson(self):
-        return list(map(lambda obj: obj.serialize(), self.favoritosPerson))
+def obtener_Favoritos(self):
+        return list(map(lambda obj: obj.serialize(), self.Favoritos))
 
 def obtener_favoritosPlanet(self):
         return list(map(lambda obj: obj.serialize(), self.favoritosPlanet))
@@ -54,8 +50,8 @@ class Planetas(db.Model):
     forma = db.Column(db.String(250),  unique=False, nullable=False)
     características= db.Column(db.String(80), unique=False, nullable=False)
     especies_que_habitan = db.Column(db.String(250), unique=False, nullable=False)
-    personaje_id = db.Column(db.Integer, db.ForeignKey('personaje.id'))
-    planetas = db.relationship("Planetas", secondary= favoritosPlanet)
+    # personaje_id = db.Column(db.Integer, db.ForeignKey('personaje.id'))
+    favoritos = db.relationship('Favorito',secondary= favoritos)
     def serialize(self):
         return {
             "id": self.id,
@@ -72,6 +68,8 @@ class Personajes(db.Model):
     personalidad = db.Column(db.String(250),  unique=False, nullable=False)
     genero = db.Column(db.String(250),  unique=False, nullable=False)
     características= db.Column(db.String(80), unique=False, nullable=False)
+    favoritos = db.relationship('Favorito',secondary= favoritos)
+
     def serialize(self):
         return {
             "id": self.id,
